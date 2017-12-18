c68dbb017cc804d698b0cdd7615bdb6fb4c28562
Your Name <you@example.com>
2017-12-18 16:29:29 +0900

    k_o.h

---
+++ K_O.h
@@ -0,0 +1,31 @@
+void k_O(int i, int j){
+    int l;
+    while(1){
+        if(answer[i][j] == '1'){
+           user[i][j] = 'O';
+                
+            for(i=0; i<=8; i++){
+                for(j=0; j<=8; j++){
+                    printf("%2c", user[i][j]);
+                }
+                printf("\n");    
+            }
+            printf("\n");
+            printf("  The O,X input was correct.\n\n");
+            count-=1;
+            break;
+        }
+        else {
+            for(i=0; i<=8; i++){
+                for(j=0; j<=8; j++){
+                    printf("%2c", user[i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+            life-=1;
+            printf("  The O,X input was incorrect. There are %d remaining games of life. \n\n", life);
+            break;
+        }
+    }
+}
